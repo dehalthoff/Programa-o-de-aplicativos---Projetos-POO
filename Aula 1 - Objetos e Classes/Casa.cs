9776117daea659d1b46050c6ba6891a933a6f7bd
André Althoff <dehalthoff@hotmail.com>
2020-11-04 20:54:52 -0300

    Adicionar arquivos de projeto.

---
+++ Aula 1 - Objetos e Classes/Casa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_1___Objetos_e_Classes
+{
+    class Casa
+    {
+        public int numComodos;
+        public int numPortas;
+        public double preco;
+        public string endereco;
+        public string cor;
+
+        public void imprimirCaracteristicas()
+        {
+            Console.WriteLine($"Numeros dos Comodos: {numComodos}\nNumeros das Portas: {numPortas}\n" +
+                $"Endereço: {endereco}\nCor: {cor}");
+        }
+        public void ImprimirPreco()
+        {
+            Console.WriteLine($"O valor da casa é: R${preco}");
+        }
+    }
+}
---
+++ Aula 1 - Objetos e Classes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_1___Objetos_e_Classes
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            //william e golle são objetos da classe Funcionario
+            //Funcionario william = new Funcionario();
+            //william.nome = "william chenta";
+            //william.email = "email@email.com";
+            //william.idade = 44;
+
+            //Console.WriteLine($"o telefone do {william.nome} é: {william.telefone}");
+
+            //Funcionario golle = new Funcionario();
+            //golle.nome = "jorge golle";
+            //golle.idade = 40;
+            //golle.telefone = "444-4444";
+
+            //Console.WriteLine($"Deseja imprimir a idade do {william.nome} ?");
+            //if (bool.Parse(Console.ReadLine()))
+            //{
+            //    Console.WriteLine($"a idade do {william.nome} é: {william.idade}");
+            //}
+
+            Casa casas = new Casa();
+            casas.numComodos = 2;
+            casas.numPortas = 4;
+            casas.preco = 200000.00;
+            casas.endereco = "Rua dos Alfeneiros, nº4";
+            casas.cor = "Tijolo a vista";
+
+            casas.imprimirCaracteristicas();
+
+            Console.WriteLine("valor da casa!");
+           
+                casas.ImprimirPreco();
+            Console.ReadKey();
+        }
+    }
+}
